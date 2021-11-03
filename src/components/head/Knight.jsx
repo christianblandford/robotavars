d8f65dd0f74716206bcad84969e8357cb3db60d5
Christian Blandford <christianblandford@me.com>
2021-11-03 09:56:51 -0700

    Added round and knight head types

---
+++ src/components/head/Knight.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Knight = () => {
+  return (
+    <g className="Knight" transform="translate(84, 68)">
+      <path
+        d="M131 120.66V48.4C131 26.63 102.45 1 66.56 1S1 26.63 1 48.4v72.26l65.6 27.92z"
+        fill="#a2a1a1"
+        stroke="#414042"
+        strokeMiterlimit={10}
+        strokeWidth={2}
+      />
+    </g>
+  )
+}
