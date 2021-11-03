38be85d69e32faf6083331c57bf6a14b54f4d656
Christian Blandford <christianblandford@me.com>
2021-11-03 09:48:47 -0700

    Fixed clothing Y index bug
    Bug where clothing was ~10 pixels above the body element has been fixed.

--- src/components/Clothes/index.js
+++ src/components/Clothes/index.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import { Piece } from 'avataaars'
+import React from "react";
+import { Piece } from "avataaars";
 
 export const Clothes = ({ size, type, color, graphic }) => {
   const clothing = React.cloneElement(
     <Piece
       pieceType="clothe"
       pieceSize={size}
-      clotheType={type || 'ShirtCrewNeck'}
-      clotheColor={color || 'Black'}
-      graphicType={graphic || 'Pizza'}
+      clotheType={type || "ShirtCrewNeck"}
+      clotheColor={color || "Black"}
+      graphicType={graphic || "Pizza"}
+      viewBox={"0 0 264 270"}
     />
-  )
-  return clothing
-}
+  );
+  return clothing;
+};
